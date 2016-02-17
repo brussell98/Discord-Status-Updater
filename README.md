0697ce6b991bcb703bd31023d92f2388e1ab8f0d
Brandon Russell <russellbrandon1999@gmail.com>
2016-02-16 22:36:37 -0600

    mpc-hc support
    also check if status is same before updating

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Discord Status Updater
 
-A status updater for Discord using [Discord.js](https://github.com/hydrabolt/discord.js/).
+A status updater for Discord using NodeJS and [Discord.js](https://github.com/hydrabolt/discord.js/).
 
-[Website](http://brussell98.github.io/Discord-Status-Updater)   
+[Website](http://brussell98.github.io/Discord-Status-Updater/)   
 [Get the latest version here](https://github.com/brussell98/Discord-Status-Updater/releases/latest)   
 
-# How to use:
+## How to use:
 Install Python 2.7.X   
 Install NodeJS 5.X.X   
 (Make sure you add both of those to your PATH. It's in the installer options.)
@@ -13,6 +13,13 @@ Install NodeJS 5.X.X
 Run `npm i` in the root directory (`C:\...\Discord-Status-Updater\> npm i`)   
 If you don't have a C++ compiler you will see some warnings related to `node-opus`, but you can ignore those.   
 Using `config.json.example` make a new file called `config.json` with your email and password.   
-Now just open `run.bat`!
+Now just open `run.bat` or run `node Status-Updater.js --harmony`
 
 **Note:** You won't see your updated status but others will.
+
+---
+
+## Extra Features:
+
+>Automatic detection for MPC-HC (Windows only)   
+>To enable/disable this type `auto`
--- Status-Updater.js
+++ Status-Updater.js
@@ -5,19 +5,30 @@ try { var config = require("./config.json");
 } catch (error) { console.log("Config not found"); }
 
 var bot = new discord.Client();
+var auto = false;
+var currentGame = "";
 
 bot.on("ready", () => {
 	console.log("Discord Status Updater ready to use! Logged in as " + bot.user.username);
 	console.log("Press Ctrl+D to exit");
 
+	setInterval(() => {
+		autoUpdate();
+	}, 30000);
+
 	var rl = readline.createInterface(process.stdin, process.stdout);
 
-	rl.setPrompt("Status> ");
+	rl.setPrompt("> ");
 	rl.prompt();
 
 	rl.on("line", (line) => {
 		if (line.trim() == "") { bot.setPlayingGame(null);
-		} else { bot.setPlayingGame(line); }
+		} else if (line.trim() == "auto") {
+			if (!auto) { auto = true; autoUpdate();
+			} else { auto = false; bot.setPlayingGame(null); }
+		} else {
+			if (line != currentGame) { bot.setPlayingGame(line); }
+		}
 		rl.prompt();
 	}).on("close", () => {
 		console.log("\nLogged out");
@@ -31,4 +42,51 @@ if (config.email && config.password) { bot.login(config.email, config.password);
 } else {
 	if (!config.email) { console.log("email not found in config"); }
 	if (!config.password) { console.log("password not found in config"); }
+}
+
+function autoUpdate() {
+	if (auto) {
+		getTasks();
+	}
+}
+
+function getTasks() {
+	var spawn = require("child_process").spawn;
+	var tl = spawn("tasklist", ["-v", "/fo", "csv"]);
+	var processes = [];
+
+	tl.stdout.on("data", (data) => {
+		processes.push(data);
+	});
+	tl.stderr.on("data", (data) => {
+		console.log(`stderr: ${data}`);
+	});
+	tl.on("close", (code) => {
+		checkForMatch(processes.join("").split("\r\n"));
+	});
+}
+
+function checkForMatch(list) {
+	for (var i = 0; i <= list.length; i++) {
+		if (i == list.length) {
+			if (currentGame !== "") {
+				bot.setPlayingGame(null);
+				currentGame = "";
+				console.log("Unset game");
+			}
+		} else if (list[i].startsWith("\"mpc-hc.exe")) {
+			if (/:\d\d",".+"$/.exec(list[i]) !== null) {
+				var title = /:\d\d",".+"$/.exec(list[i])[0].substr(6).replace(/"$/, "");
+				if (title !== "N/A") {
+					title = title.replace(/\[[^\]]+\]/g, "").replace(/_/g, " ").replace(/\.[a-zA-Z]{3,5}$/, "").trim();
+					if (title != currentGame) {
+						bot.setPlayingGame(title);
+						console.log(title);
+						currentGame = title;
+						i = list.length + 1;
+					} else { i = list.length + 1; }
+				}
+			}
+		}
+	}
 }
